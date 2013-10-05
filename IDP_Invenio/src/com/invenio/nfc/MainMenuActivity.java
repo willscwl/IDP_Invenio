58fb7a59de4d951123e8ded4da46f91b5c765964
willscwl <wl.chung.2011@smu.edu.sg>
2013-10-05 11:05:51 +0800

    Update Main Menu

--- IDP_Invenio/src/com/invenio/nfc/MainMenuActivity.java
+++ IDP_Invenio/src/com/invenio/nfc/MainMenuActivity.java
@@ -1,8 +1,12 @@
 package com.invenio.nfc;
 
 
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
@@ -11,16 +15,26 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
 import com.invenio.nfc.LoanItemLogic;
 
 public class MainMenuActivity extends Activity {
-
+	private NfcAdapter mAdapter;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		try {
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.activity_mainmenu);
 			//getActionBar().setDisplayHomeAsUpEnabled(true);
+			
+			//Check for NFC settings 
+			mAdapter = NfcAdapter.getDefaultAdapter(this);
+		        if (mAdapter == null) {
+		            //showMessage("test", "test");
+		            finish();
+		            return;
+		        }
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -87,4 +101,36 @@ public class MainMenuActivity extends Activity {
             return super.onOptionsItemSelected(item);
         }
     }
+	
+	protected void onResume() {
+        super.onResume();
+        if (mAdapter != null) {
+            if (!mAdapter.isEnabled()) {
+                showWirelessSettingsDialog();
+            }
+
+        }
+    }
+	
+	private void showWirelessSettingsDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //builder.setCancelable(false);
+        builder.setMessage("NFC is disabled. Click OK to go to the wireless settings to enable it.");
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(Settings.ACTION_WIRELESS_SETTINGS);
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        builder.create().show();
+        
+        //return;
+    }
+
+	
 }
